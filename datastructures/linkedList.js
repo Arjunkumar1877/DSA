652d6e2f5574d311ab53569185d2ab46761e7828
Arjun P S <arjunpadinjarethil33@gmail.com>
2024-08-09 22:32:36 +0530

    funtion to add to linkedlist at start

---
+++ datastructures/linkedList.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = null;
+  }
+
+  addAtStart(data) {
+    let newNode = new Node(data);
+
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return;
+  }
+}
