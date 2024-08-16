9f21fcef4db02b6862094f4bf3f6ed0efdb4f660
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-16 14:29:20 +0530

    js updated string methods

---
+++ javascript/StringFunctions.js
@@ -0,0 +1,335 @@
+// CharAt();
+// const str = 'Hello';
+// console.log(str.charAt(1));
+
+
+
+
+
+
+
+
+
+
+
+// CharCodeat()
+// const str = 'Hello';
+// console.log(str.charCodeAt(1)); 
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = '8'; 
+// console.log(str.codePointAt(0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str1 = 'Hello';
+// const str2 = ' World';
+// console.log(str1.concat(str2)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.endsWith('World')); 
+// console.log(str.endsWith('Hello')); 
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.includes('World')); 
+// console.log(str.includes('world')); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.indexOf('o')); 
+// console.log(str.indexOf('WOO'));
+
+
+
+
+
+
+
+
+// const str1 = 'a';
+// const str2 = 'b';
+// console.log(str1.localeCompare(str2));
+// console.log(str2.localeCompare(str1));
+// console.log(str1.localeCompare(str1)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello world';
+// const regex = /o/g;
+// console.log(str.match(regex));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// const regex = /o/g;
+// const matches = str.matchAll(regex);
+// console.log(Array.from(matches)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = '\u004F\u030C';
+// console.log(str.normalize()); 
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello';
+// console.log(str.padEnd(6, '!')); 
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello';
+// console.log(str.padStart(10, '!')); 
+
+
+
+
+
+
+
+// const str = ' Hello';
+// console.log(str.repeat(3)); 
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.replace('World', 'Universe'));
+
+
+
+
+
+
+
+
+// const str = 'Hello World, World!';
+// console.log(str.replaceAll('World', 'Universe')); 
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.search(/o/)); 
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.split('')); 
+// let str2 = str.split('');
+// console.log(str2.join(" "));
+
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.startsWith('Hello')); 
+// console.log(str.startsWith('World'));
+
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.substring(0, 5));
+// console.log(str.substring(6));    
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.toLocaleLowerCase()); 
+
+
+
+
+
+
+
+
+// const str = 'Hello World';
+// console.log(str.toLocaleUpperCase()); 
+
+
+
+
+
+
+
+
+// const str = '   Hello World   ';
+// console.log(str.trim()); 
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = '   Hello World   ';
+// console.log(str.trimEnd()); 
+
+
+
+
+
+
+
+
+
+
+
+// const str = '   Hello World   6';
+// console.log(str.trimStart());
+
+
+
+
+
+
+
+
+
+
+// const str = new String('Hello World');
+// console.log(str.valueOf()); 
