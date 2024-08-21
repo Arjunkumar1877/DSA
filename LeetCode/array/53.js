93278543a0922c1c3fac826b72b68d63583d4f04
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-21 20:49:11 +0530

    leetcode array 283

--- LeetCode/array/53.js
+++ LeetCode/array/53.js
@@ -1,20 +1,24 @@
 // Given an integer array nums, find the  subarray with the largest sum, and return its sum.
 
 
-const nums =[-2,1,-3,4,-1,2,1,-5,4];
+const nums = [-2,1,-3,4,-1,2,1,-5,4];
 
 // Method - 1
-function maxSubArray(nums){
-    function helper(i){
-        if(i === 0) return nums[0];
-        let prevMax = helper(i - 1);
-        return Math.max(nums[i], nums[i] + prevMax);
+function MaxSubArray(nums){
+    let currentMax = nums[0];
+    let globalMax = nums[0];
+
+    for(let i = 0; i < nums.length - 1; i++){
+        currentMax = Math.max(nums[i], currentMax + nums[i]);
+
+        if(currentMax > globalMax){
+            globalMax = currentMax;
+        }
     }
-    return helper(nums.length - 1);
+    return globalMax;
 }
 
-console.log(maxSubArray(nums));
-
+console.log(MaxSubArray(nums));
 
 
 // Method - 2;
