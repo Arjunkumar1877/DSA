ac1105ece5f962b2dbb1048ca3124e86ed2ba498
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-21 16:59:56 +0530

    leetcode string 2108

--- LeetCode/string/2108.js
+++ LeetCode/string/2108.js
@@ -27,11 +27,36 @@ const words2 = ["abc","car","adao","racecar","cool"];
 
 
 // Method - 2
-function firstPalindrom(words){
+function findFirstPalindrom(words){
   for(let i = 0; i < words.length; i++){
-    let w 
+    let w = words[i];
+    const result = check(w);
+
+    if(!result){
+        continue;
+    }else{
+        return result;
+    }
   }
+  return "no palindrom";
 }
+
+
+function check(word, lb= 0, ub= word.length-1){
+ if(lb > ub){
+    return word;
+ }
+
+ if(word[lb] !== word[ub]){
+    return false;
+ }else{
+    return check(word, lb += 1, ub -= 1);
+ }
+}
+
+
+console.log(findFirstPalindrom(words));
+
 
 
 
