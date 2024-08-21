1eb15ae973539337fa8337b41753ce79b04650f2
Arjun P S <arjunpadinjarethil33@gmail.com>
2024-08-21 12:53:31 +0530

    Array problems updates

---
+++ javascript/ArrayProblems.js
@@ -0,0 +1,39 @@
+//find the number of wovels in this string
+
+const str =
+  "qwertyuiopasdfghjklasdfghjkqwertyuioasdfghjwertyuiwsdxcwedfzxcghujnb";
+const set = new Set(["a", "e", "i", "o", "u"]);
+
+const obj = {};
+
+for (let i = 0; i < str.length; i++) {
+  if (set.has(str[i])) {
+    if (obj[str[i]]) {
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+}
+
+//console.log(obj);
+
+//using recursion
+
+function getVowels(str, i = 0, obj = {}) {
+  if (i === str.length) {
+    return obj;
+  }
+
+  if (set.has(str[i])) {
+    if (obj[str[i]]) {
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+    }
+  }
+
+  return getVowels(str, i + 1, obj);
+}
+
+console.log(getVowels(str));
