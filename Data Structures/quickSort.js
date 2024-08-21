883e55d3cf59d2b8f977610fe6c4088d994d2aed
Arjun P S <arjunpadinjarethil33@gmail.com>
2024-08-21 22:09:42 +0530

    quick sort file edited

--- Data Structures/quickSort.js
+++ Data Structures/quickSort.js
@@ -1,5 +1,4 @@
-
-let arr = [5, 7, 8, 0, 1, 3, 4, 0,9]
+let array = [5, 7, 8, 0, 1, 3, 4, 0, 9];
 
 // function QuickSort(arr){
 //     if(arr.length < 2){
@@ -21,45 +20,41 @@ let arr = [5, 7, 8, 0, 1, 3, 4, 0,9]
 //     return [...QuickSort(left), pivot, ...QuickSort(right)];
 // }
 
-
 // console.log(QuickSort(arr))
 
-function Sort(arr, lb, ub){
-   let i = lb;
-   let j = ub;
-   let pivot = arr[lb];
+function Sort(arr, lb, ub) {
+  let i = lb;
+  let j = ub;
+  let pivot = arr[lb];
 
-   while(i <= j){
-     
-    while(i <= j && pivot >= arr[i]){
-        i++;
+  while (i <= j) {
+    while (i <= j && pivot >= arr[i]) {
+      i++;
     }
 
-    while(i <= j && pivot < arr[j]){
-        j--;
+    while (i <= j && pivot < arr[j]) {
+      j--;
     }
 
-    if(i < j){
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+    if (i < j) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-   }
+  }
 
-   [arr[lb], arr[j]] = [arr[j], arr[lb]];
+  [arr[lb], arr[j]] = [arr[j], arr[lb]];
 
-   return j;
-
+  return j;
 }
 
+function QuickSort(arr, lb, ub) {
+  if (lb >= ub) {
+    return;
+  }
 
-function QuickSort(arr, lb, ub){
-    if(lb >= ub){
-        return;
-    }
-
-    let mid = Sort(arr, lb, ub);
-    QuickSort(arr, lb, mid-1);
-    QuickSort(arr, mid+1, ub);
-    return arr;
+  let mid = Sort(arr, lb, ub);
+  QuickSort(arr, lb, mid - 1);
+  QuickSort(arr, mid + 1, ub);
+  return arr;
 }
 
 // console.log(QuickSort(arr, 0, arr.length-1))
